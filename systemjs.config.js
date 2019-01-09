eb1ac7e514991cf06195d8dd6b62f6c3c5ed1246
Stan Silvert <ssilvert@redhat.com>
2019-01-09 16:16:59 -0500

    Add system.config.js

---
+++ systemjs.config.js
@@ -0,0 +1,27 @@
+/**
+ * System configuration for Angular samples
+ * Adjust as necessary for your application needs.
+ */
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': './node_modules/'
+    },
+    
+    // map tells the System loader where to look for things
+    map: {
+      'react': 'npm:react/umd/react.development.js',
+      'react-dom': 'npm:react-dom/umd/react-dom.development.js',
+    },
+    
+    bundles: {
+        
+      },
+      
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      
+    }
+  });
+})(this);
